e31d381f945b2cdaa2a71fc669de1e35150f9010
Josh-M-Dev <joshmpaka@gmail.com>
2019-08-24 06:56:32 +0300

    Added sign in and sign up with Email and password

--- app/src/main/java/com/justice/justiceapp/MainActivity.java
+++ app/src/main/java/com/justice/justiceapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.justice.justiceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
