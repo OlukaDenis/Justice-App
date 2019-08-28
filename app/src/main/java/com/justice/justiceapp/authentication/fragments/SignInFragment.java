abaaf6eb61a5e7f5ca07910a18b330b918d09396
OlukaDenis <olukadeno@gmail.com>
2019-08-29 01:56:03 +0300

    Merged the branches

--- app/src/main/java/com/justice/justiceapp/authentication/fragments/SignInFragment.java
+++ app/src/main/java/com/justice/justiceapp/authentication/fragments/SignInFragment.java
@@ -1,20 +1,39 @@
 package com.justice.justiceapp.authentication.fragments;
 
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.justice.justiceapp.MainActivity;
 import com.justice.justiceapp.R;
 import com.justice.justiceapp.authentication.ForgotPasswordActivity;
+
+import java.util.concurrent.Executor;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +44,11 @@ public class SignInFragment extends Fragment implements View.OnClickListener {
 
     private TextInputEditText userNameOrEmail;
     private TextInputEditText userPassword;
+    private static final int RC_SIGN_IN = 433;
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+    private ProgressBar progressBar;
+    private String emailReset;
 
     private Button buttonForgotPassword;
     private Button buttonLogin;
@@ -41,6 +65,8 @@ public class SignInFragment extends Fragment implements View.OnClickListener {
         View view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_sign_in,
                 container, false);
 
+        mAuth = FirebaseAuth.getInstance();
+
         userNameOrEmail = view.findViewById(R.id.signInUsernameOrEmail);
         userPassword = view.findViewById(R.id.signInPassword);
 
@@ -50,6 +76,17 @@ public class SignInFragment extends Fragment implements View.OnClickListener {
         buttonForgotPassword.setOnClickListener(this);
         buttonLogin.setOnClickListener(this);
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(getActivity(), gso);
+
+        if (mAuth.getCurrentUser() != null && mAuth.getCurrentUser().isEmailVerified()) {
+            startActivity(new Intent(getContext(), MainActivity.class));
+            getActivity().finish();
+        }
         return view;
     }
 
@@ -65,15 +102,139 @@ public class SignInFragment extends Fragment implements View.OnClickListener {
             }
             case R.id.buttonLogin: {
                 Log.d(TAG, "onClick: login option");
-//                Todo: Login logic will be here
-                startActivity(new Intent(getActivity(), MainActivity.class));
-                getActivity().overridePendingTransition(android.R.anim.fade_in,
-                        android.R.anim.fade_out);
-                getActivity().finish();
-
+                signIn();
+                break;
+            }
+            case R.id.loginWithGoogleButton: {
+                Log.d(TAG, "onClick: google login");
+                googleLogin();
                 break;
             }
         }
 
+    }
+
+    public void signIn(){
+        String email = userNameOrEmail.getText().toString();
+        final String password = userPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //authenticate user
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the mAuth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            // there was an error
+                            if (password.length() < 6) {
+                                userPassword.setError(getString(R.string.minimum_password));
+                            } else {
+                                Toast.makeText(getContext(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            if (mAuth.getCurrentUser().isEmailVerified()){
+                                Intent intent = new Intent(getContext(), MainActivity.class);
+                                startActivity(intent);
+                                getActivity().overridePendingTransition(android.R.anim.fade_in,
+                                        android.R.anim.fade_out);
+                                getActivity().finish();
+
+                            }
+                            else {
+                                Toast.makeText(getContext(), "Please verify your email address ", Toast.LENGTH_LONG).show();
+                            }
+
+
+                        }
+                    }
+                });
+    }
+
+    private void googleLogin() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener((Executor) this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(getContext(), "Authentication Failed", Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                    }
+                });
+    }
+
+    @Override
+    public void onStart() {
+        // Check if user is signed in (non-null) and update UI accordingly.
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+
+            if (user.isEmailVerified()){
+
+                Intent intent = new Intent(getContext(),MainActivity.class);
+                startActivity(intent);
+                Toast.makeText(getContext(), "Logged in", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(getContext(), "Check your Email for verification link", Toast.LENGTH_SHORT).show();
+
+            }
+
+        }
+        else {
+            //Intent intent = new Intent(getContext(),LoginActivity.class);
+           // startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/justice/justiceapp/authentication/fragments/SignUpFragment.java
+++ app/src/main/java/com/justice/justiceapp/authentication/fragments/SignUpFragment.java
@@ -4,15 +4,24 @@ package com.justice.justiceapp.authentication.fragments;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.justice.justiceapp.R;
 import com.justice.justiceapp.authentication.AuthenticationActivity;
 
@@ -31,6 +40,8 @@ public class SignUpFragment extends Fragment implements View.OnClickListener{
 
 
     private Button buttonSignUp;
+    private FirebaseAuth auth;
+    private TextView txt_Resend_Verification;
 
     public SignUpFragment() {
         // Required empty public constructor
@@ -44,6 +55,8 @@ public class SignUpFragment extends Fragment implements View.OnClickListener{
         View view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_sign_up,
                 container, false);
 
+        auth = FirebaseAuth.getInstance();
+
         firstName = view.findViewById(R.id.signUpFirstName);
         lastName = view.findViewById(R.id.signUpLastName);
         phoneNumber = view.findViewById(R.id.signUpPhoneNumber);
@@ -52,6 +65,9 @@ public class SignUpFragment extends Fragment implements View.OnClickListener{
 
         buttonSignUp = view.findViewById(R.id.buttonSignUp);
         buttonSignUp.setOnClickListener(this);
+
+        txt_Resend_Verification = view.findViewById(R.id.txt_resend);
+        txt_Resend_Verification.setOnClickListener(this);
 
 
         return view;
@@ -64,9 +80,117 @@ public class SignUpFragment extends Fragment implements View.OnClickListener{
         int viewId = view.getId();
         switch (viewId){
             case R.id.buttonSignUp:
-                //        Todo: Sign up logic will be here User should be taken to sign in fragment
+                signUp();
                 break;
+
+            case R.id.txt_resend:
+                //resendVerification();
+                break;
+        }
+
+    }
+
+    private void resendVerification() {
+        if(auth.getCurrentUser() != null){
+            auth.getCurrentUser().reload(); // reloads user fields, like emailVerified:
+            if (!auth.getCurrentUser().isEmailVerified()) {
+                auth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(getContext(), "Verification Email Resent", Toast.LENGTH_LONG).show();
+                        }
+                        else
+                            Toast.makeText(getContext(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+            } else {
+                //startActivity(new Intent(getContext(), LoginActivity.class));
+                Toast.makeText(getContext(), "Login to your account", Toast.LENGTH_SHORT).show();
             }
+
         }
     }
+
+    private void signUp() {
+        final String email = emailAddress.getText().toString().trim();
+        final String mpassword = password.getText().toString().trim();
+        final String number = phoneNumber.getText().toString().trim();
+        final String fname = firstName.getText().toString().trim();
+        final String lname = lastName.getText().toString().trim();
+
+        if (TextUtils.isEmpty(fname)) {
+            Toast.makeText(getContext(), "Enter your first name", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(lname)) {
+            Toast.makeText(getContext(), "Enter your last name", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(number)) {
+            Toast.makeText(getContext(), "Enter your phone number!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(mpassword)) {
+            Toast.makeText(getContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (password.length() < 6) {
+            Toast.makeText(getContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        //create user
+        auth.createUserWithEmailAndPassword(email, mpassword)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (task.isSuccessful()){
+                            auth.getCurrentUser().sendEmailVerification()
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                                                Toast.makeText(getContext(),
+                                                        "User with this email already exist.", Toast.LENGTH_SHORT).show();
+                                            }
+
+                                            else if (task.isSuccessful()){
+                                                Toast.makeText(getContext(), "Registered successfully. Please check your email for verification ", Toast.LENGTH_SHORT).show();
+                                                emailAddress.setText("");
+                                                password.setText("");
+                                                firstName.setText("");
+                                                lastName.setText("");
+                                                phoneNumber.setText("");
+                                                //startActivity(new Intent(getContext(), LoginActivity.class));
+                                            }
+                                            else{
+                                                Toast.makeText(getContext(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                            }
+
+                                        }
+                                    });
+                        }
+
+                    }
+                });
+    }
+}
+
+
 
